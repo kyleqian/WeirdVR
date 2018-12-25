cd5d974a38c0081553c01fa38d9343df31adf99d
Kyle Qian <kylecqian@gmail.com>
2018-12-24 23:40:30 -0800

    Initial commit

---
+++ Assets/_SCRIPTS/InvertCameraRotation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class InvertCameraRotation : MonoBehaviour
+{
+    [SerializeField] bool InvertX;
+    [SerializeField] bool InvertY;
+    [SerializeField] bool InvertZ;
+
+    Transform cameraTransform;
+    Transform cameraParentTransform;
+    Vector3 prevEulerAngles;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        cameraParentTransform = cameraTransform.parent;
+        prevEulerAngles = cameraTransform.localEulerAngles;
+    }
+
+    void Update()
+    {
+        Vector3 eulerAngleDiff = cameraTransform.localEulerAngles - prevEulerAngles;
+        if (!InvertX)
+        {
+            eulerAngleDiff.x = 0;
+        }
+        if (!InvertY)
+        {
+            eulerAngleDiff.y = 0;
+        }
+        if (!InvertZ)
+
+        {
+            eulerAngleDiff.z = 0;
+        }
+        cameraParentTransform.eulerAngles -= 2 * eulerAngleDiff;
+        prevEulerAngles = cameraTransform.localEulerAngles;
+    }
+}
---
+++ Assets/_SCRIPTS/SitStill.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SitStill : MonoBehaviour
+{
+    [SerializeField] TextMeshPro textMesh;
+
+    Vector3 prevCameraRotation;
+
+    void Start()
+    {
+        prevCameraRotation = Camera.main.transform.localEulerAngles;
+    }
+
+    void Update()
+    {
+        Vector3 currCameraRotation = Camera.main.transform.localEulerAngles;
+        textMesh.text = Vector3.Distance(prevCameraRotation, currCameraRotation).ToString("F6");
+        prevCameraRotation = currCameraRotation;
+    }
+}
